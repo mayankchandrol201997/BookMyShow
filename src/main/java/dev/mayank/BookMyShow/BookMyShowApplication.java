e0c2cb1d2be3c335189b9319b54326898527ce88
Mayank Chandrol <mayankchandrol201997@gmail.com>
2024-03-06 21:22:34 +0530

    initial setup for bookmyshow

---
+++ src/main/java/dev/mayank/BookMyShow/BookMyShowApplication.java
@@ -0,0 +1,14 @@
+package dev.mayank.BookMyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		System.out.println("MAYANK CHANDROL");
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
